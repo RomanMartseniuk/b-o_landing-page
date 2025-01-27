c5381b47139285f56be8291786315bb10177f264
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2025-01-27 21:32:28 +0200

    make build

---
+++ index.895c4c33.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("menu-button"),t=document.getElementById("menu"),l=document.getElementById("call-link");e.onclick=function(){l.classList.toggle("invisible"),e.classList.toggle("active"),t.classList.toggle("active"),document.querySelector("header").classList.toggle("header--bg-white"),document.body.classList.toggle("unscrollable")};
+//# sourceMappingURL=index.895c4c33.js.map
